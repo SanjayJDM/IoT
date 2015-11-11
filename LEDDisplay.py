5fe4c87f6d286b3b6ba0f1e89d6173cb591bbbc8
Sanjay <sanjayjdm@gmail.com>
2015-11-11 13:30:52 +0000

    Added comments

--- LEDDisplay.py
+++ LEDDisplay.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask #flask to enable this as a web service invocation
 import RPi.GPIO as GPIO
 import time
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/") # root
 def hello():
  try:
   GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(7,GPIO.OUT)
-  GPIO.setup(11,GPIO.OUT)
+  GPIO.setup(7,GPIO.OUT) # red LED 
+  GPIO.setup(11,GPIO.OUT) # blue LED
+  #Switch ON/Off every 2 secs
   for x in range(0,3):
    GPIO.output(7,True)
    time.sleep(2) 
@@ -21,4 +22,4 @@ def hello():
    GPIO.cleanup()
  return "Lights Off"
 if __name__=="__main__":
-  app.run(host='0.0.0.0', port=80, debug=True)
+  app.run(host='0.0.0.0', port=80, debug=True) # server runs on the default port
